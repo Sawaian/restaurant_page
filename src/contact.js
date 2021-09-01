f2c5c57b516592406acacadced659d3d529660e5
Keith <Keith.macario@gmail.com>
2021-08-31 22:27:48 -0700

    set UID.

--- src/contact.js
+++ src/contact.js
@@ -14,25 +14,26 @@ let contactDiv = contactFactory();
 
 
 
-
-
-
-
 function contact(){
-
-                    let googleMap = document.createElement("div");
+                    let googleMap= document.createElement("img");
+                    googleMap.classList.add("map");
+                    googleMap.src = "toastiesLocation.png";
                     googleMap.setAttribute("id", "map");
                     contactDiv.contactPage.appendChild(googleMap);
-                    contactDiv.contactPage.appendChild(paragraphFunc("123 BreadLane, Washington"));
-                    contactDiv.contactPage.appendChild(paragraphFunc( "555-Get-Loaf"));
+
+                    contactDiv.contactPage.appendChild(googleMap);
+                    contactDiv.contactPage.appendChild(paragraphFunc("123 BreadLane, Washington", "contact" + 0));
+                    contactDiv.contactPage.appendChild(paragraphFunc( "555-Get-Loaf", "contact" + 1));
 
 
                 }
 
-        function paragraphFunc(info){
+        function paragraphFunc(info, uid){
+                  
                  let paragraph = document.createElement("p");
                     paragraph.textContent = info;
                     paragraph.setAttribute("class", "text");
+                    paragraph.setAttribute("id", uid);
              return paragraph;
             }
 
