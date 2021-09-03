e5d6747f6301f68102fb08298201ceef713f1b90
Keith <Keith.macario@gmail.com>
2021-09-02 22:55:15 -0700

    Added gif to front page.

--- src/homepage.js
+++ src/homepage.js
@@ -12,16 +12,22 @@ return { homePageBody }
 
 
 function homePage(){
-homeFact.homePageBody.appendChild(paragraphFunc("Welcome to Toasties!"));
-homeFact.homePageBody.appendChild(paragraphFunc( "We serve the freshest bread at the best prices!"));
+homeFact.homePageBody.appendChild(paragraphFunc("Welcome to Toasties!", "home" + 0));
+homeFact.homePageBody.appendChild(paragraphFunc( "We serve the freshest bread at the best prices!", "home" + 1));
+
+let homeGif = document.createElement("img");
+        homeGif.setAttribute("id","homeGif");
+        homeGif.src = "eatUp.gif";
+homeFact.homePageBody.appendChild(homeGif);
 
 }
 
-function paragraphFunc(info){
+function paragraphFunc(info, uid){
 
     let paragraph = document.createElement("p");
     paragraph.textContent = info;
     paragraph.setAttribute("class", "text");
+    paragraph.setAttribute("id", uid);
     return paragraph;
 
 }
